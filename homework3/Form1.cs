f828f6608333d0e05bab658d90a787a76adb8dcd
Gilgamesh Aljabari <galito043@gmail.com>
2025-11-06 23:10:45 +0200

    Add homework 3,4

---
+++ homework3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int[] numbers = new int[30];
+            for(int i = 0; i < 30; i++)
+            {
+                numbers[i] = rand.Next(50);
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] * 3;
+
+            }
+            listBox1.Items.AddRange(numbers.Select( n => n.ToString()).ToArray());
+        }
+    }
+}
---
+++ homework4/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textToCheck = textBox1.Text;
+
+            if (radioButton1.Checked)
+            {
+                if (Regex.IsMatch(textToCheck, "^\\d{2}(0[1-9]|1[0-2]|4[1-9]|5[0-2])(0[1-9]|[12]\\d|3[01])\\d{4}$"))
+                {
+                    MessageBox.Show("ЕГН е валидно");
+                }
+                else
+                {
+                    MessageBox.Show("ЕГН не е валидно!");
+                }
+            }
+            else if (radioButton2.Checked) {
+                if (Regex.IsMatch(textToCheck, "^[789]\\d{9}$"))
+                {
+                    MessageBox.Show("ЛНЧ е валиден");
+                }
+                else
+                {
+                    MessageBox.Show("ЛНЧ не е валиден!");
+                }
+            }
+            else if (radioButton3.Checked) {
+                if (Regex.IsMatch(textToCheck, "^\\d{9}(\\d{4})?$"))
+                {
+                    MessageBox.Show("Булстатът е валиден ");
+                }
+                else
+                {
+                    MessageBox.Show("Булстат не е валиден!");
+                }
+            }
+
+            
+        }
+    }
+}

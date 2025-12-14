453c2557e8925de105d86d9aeb146a59494271dd
Gilgamesh Aljabari <galito043@gmail.com>
2025-12-14 17:10:10 +0200

    Add queries

---
+++ Tourist_Agency/Tourist_Agency/Queries.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Tourist_Agency
+{
+    partial class Queries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "clients between dates", "destinations visited by client", "search by  customers by egn", "search by customers by insurance", "search by order date" });
+            comboBox1.Location = new Point(322, 15);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(463, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(159, 23);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(628, 12);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(160, 23);
+            dateTimePicker2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(322, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(322, 136);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 24);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(281, 148);
+            dataGridView1.TabIndex = 5;
+            // 
+            // Queries
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox1);
+            Name = "Queries";
+            Text = "Form1";
+            Load += Queries_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Tourist_Agency/Tourist_Agency/Queries.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tourist_Agency
+{
+    public partial class Queries : Form
+    {
+        string connectionString = @"";
+        public Queries()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = "SELECT c.client_id, c.full_name, c.personal_id,  e.destination_country, e.destination_city FROM dbo.Orders o INNER JOIN dbo.Clients c ON o.client_id = c.client_id " +
+                            "INNER JOIN dbo.Excursions e ON o.excursion_id = e.excursion_id WHERE e.excursion_start_date >= @StartDate AND e.excursion_end_date <= @EndDate";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@StartDate", dateTimePicker1.Value);
+                        cmd.Parameters.AddWithValue("@EndDate", dateTimePicker2.Value);
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = "SELECT e.destination_country, e.destination_city, e.transport_price, e.food_accommodation_price, e.additional_package_price, c.insurance_id FROM dbo.Orders o " +
+                            "INNER JOIN dbo.Clients c ON o.client_id = c.client_id INNER JOIN dbo.Excursions e ON o.excursion_id = e.excursion_id WHERE c.personal_id = @ClientEGN";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@ClientEGN", textBox1.Text);
+
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = "SELECT o.order_number, c.full_name, c.personal_id, c.insurance_id, o.order_date, e.destination_country FROM dbo.Orders o " +
+                            "INNER JOIN dbo.Clients c ON o.client_id = c.client_id INNER JOIN dbo.Excursions e ON o.excursion_id = e.excursion_id " +
+                            "WHERE c.personal_id LIKE @SearchText";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@SearchText", textBox1.Text);
+
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = "SELECT o.order_number, c.full_name, c.personal_id, c.insurance_id, o.order_date, e.destination_country FROM dbo.Orders o " +
+                            "INNER JOIN dbo.Clients c ON o.client_id = c.client_id INNER JOIN dbo.Excursions e ON o.excursion_id = e.excursion_id " +
+                            "WHERE c.insurance_id LIKE @SearchText";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@SearchText", textBox1.Text);
+
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        string query = "SELECT o.order_number, c.full_name, c.personal_id, c.insurance_id, o.order_date, e.destination_country FROM dbo.Orders o " +
+                            "INNER JOIN dbo.Clients c ON o.client_id = c.client_id INNER JOIN dbo.Excursions e ON o.excursion_id = e.excursion_id " +
+                            "WHERE o.order_date = @SearchDate";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@SearchDate", dateTimePicker1.Value.Date);
+
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+
+        private void Queries_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
